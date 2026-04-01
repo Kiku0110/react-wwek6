79059da31ba2b84b71214b9b4b15751942700730
Kiku <likusa0110@gmail.com>
2026-04-01 14:37:39 +0800

    新增 gh-pages, base 和鎖定 axios 版本至 1.13.4

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // eslint-disable-next-line no-undef
+  base:process.env.NODE_ENV === 'production'? '/react-wwek6/':'/',
   plugins: [react()],
 })
